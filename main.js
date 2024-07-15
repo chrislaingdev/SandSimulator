0e134a7259cc2bdf191c784a22ffb393d87d3890
Chris Laing <chrislaingdev@gmail.com>
2024-07-15 10:01:18 -0500

    ironing out gravity

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let canvasContext = canvasElement.getContext("2d");
 const gridSize = 10;
 const canvasWidth = 400;
 const canvasHeight = 600;
+const runtime = 5000;
 
 class Square{
   constructor(x, y){
@@ -36,40 +37,48 @@ class Squares{
     } 
   }
 
-  activateSquare(x, y){
-    this.squares[y][x].active = true;
-    console.log(this.squares[y][x].active);
-    console.log('above is ' + this.squares[y - 1][x].active)
+  drawSquares(){
     canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
-    canvasContext.beginPath();
-    canvasContext.rect(x * gridSize, y * gridSize, gridSize, gridSize);
-    canvasContext.fillStyle='brown';
-    canvasContext.fill();
-    if(y < 59 && this.squares[y + 1][x].active === false){
-      this.squares[y][x].active = false;
-      
-      setTimeout(this.activateSquare.bind(this,x, (y + 1)), 500); 
-      
-      
-      //setTimeout(this.tryFalling.bind(this,x, y), 500);
+    for(let row = 0; row < 60; row ++){
+      for(let col = 0; col < 40; col++){
+
+        if (this.squares[row][col].active){
+          canvasContext.beginPath();
+          canvasContext.rect(col * gridSize, row * gridSize, gridSize, gridSize);
+          canvasContext.fillStyle='brown';
+          canvasContext.fill();
+
+        }
+      }
     }
     createGrid();
   }
 
-  tryFalling(x,y){
-    if(this.squares[y][x].active){
-      if(!this.squares[y + 1][x].active){
-        
-        //this.squares[y][x].active = false;
-        //console.log(this.squares[y][x].active);
-        canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        //createGrid();
-        //canvasContext.rect(x * gridSize, y * gridSize, gridSize, gridSize);
-        //canvasContext.fillStyle='green';
-        //canvasContext.fill();
-        
-        setTimeout(this.activateSquare.bind(this,x, y + 1), 500); 
+
+  activateSquare(x, y){
+    this.squares[y][x].active = true;    
+  }
+  
+  tryFallingDown(){
+    let arr = [];
+    for(let row = 0; row < 60; row ++){
+      for(let col = 0; col < 40; col++){
+        if(this.squares[row][col].active && col < 59 && !this.squares[row + 1][col].active){
+          console.log(this.squares[row][col]);
+          let coor = [row, col];
+          arr.push(coor);
+        }
       }
+    }
+    console.log(arr);//pick up HEREarr [[1,2]]
+    for(let i = 0; i <= arr.length; i++){
+      console.log("y: " + arr[i][0]);
+      console.log(arr[i][1]);
+      let y = arr[i][0];
+      let x = arr[i][1];
+      this.squares[y][x].active = false;
+      this.squares[y + 1][x].active = true;
+
     }
   }
 
@@ -119,7 +128,20 @@ canvasElement.addEventListener("mousedown", function (e) {
 
 
 
+//gridSquares.squares[0][10].active = true;
 
+//console.log(gridSquares.squares[0][10]);
 
-console.log(gridSquares.squares[0][10]);
+let running = true;
+
+const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+async function repeatedGreetingsLoop() {
+  while(running) {
+    await sleepNow(runtime)
+    gridSquares.drawSquares();
+    gridSquares.tryFallingDown();
+  }
+}
+
+repeatedGreetingsLoop();
 
